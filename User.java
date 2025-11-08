0cd4f2a56a711e454fb12d17784d756e8b4bf156
Ansh <anskaul8@gamil.com>
2025-11-08 15:22:58 +0530

    Updated

--- User.java
+++ User.java
@@ -1,1 +1,18 @@
+package BankingManagementSystem;
 
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class User {
+    private Connection connection;
+    private Scanner scanner;
+
+    public User(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+}
+}
