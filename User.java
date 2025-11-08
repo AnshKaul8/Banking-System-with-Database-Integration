f43f469c059ecbcdd2b9743e81c43a6cdf40b133
Ansh <anskaul8@gamil.com>
2025-11-08 15:31:43 +0530

    Update

--- User.java
+++ User.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+//Class User declared
 public class User {
     private Connection connection;
     private Scanner scanner;
@@ -14,5 +15,24 @@ public class User {
     public User(Connection connection, Scanner scanner){
         this.connection = connection;
         this.scanner = scanner;
+}
+
+//Register Function
+
+public void register(){
+        scanner.nextLine();
+        System.out.print("Full Name: ");
+        String full_name = scanner.nextLine();
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+        if(User_exist(email)) {
+            System.out.println("User Already Exists for this Email Address!!");
+            return;
+        }
+
+
+
 }
 }
