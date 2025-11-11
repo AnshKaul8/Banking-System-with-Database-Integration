dee8155b6f251ca2ba56ae83f819fc85a5553a8a
Harsh Sagar <harshsagar303@gmail.com>
2025-11-11 18:10:07 +0530

    Delete AccountManager.java

--- AccountManager.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class AccountManager{
-public static void main(String [] args){
-
-    System.out.println("Account Manager Testing....");
-    System.out.println("Successfully Done..");
-}
-}
