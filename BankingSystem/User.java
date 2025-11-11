819b436caa1474dd2cf18b3e6cbc4e1e8fd9b7dd
Ansh Kaul <anshkaul8@gmail.com>
2025-11-11 22:23:31 +0530

    Update User.java

--- BankingSystem/User.java
+++ BankingSystem/User.java
@@ -3,7 +3,16 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
+//Class User declaration
+public class User {
+    private Connection connection;
+    private Scanner scanner;
 
+//Constructor Initialization
+    public User(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+    }
 
 //Register Function
 
